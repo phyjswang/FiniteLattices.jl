bd609899d56d83b124f953dab78dfff4e22fa6e5
Junsen Wang <jsw@mail.ustc.edu.cn>
2025-10-29 18:00:16 +0800

    bugfix

--- src/Lattice/util.jl
+++ src/Lattice/util.jl
@@ -1,4 +1,4 @@
-function getidx(Latt<:AbstractLattice, x::Real, y::Real; forceerr = false)
+function getidx(Latt::AbstractLattice, x::Real, y::Real; forceerr::Bool = false)
     idx = findfirst(i -> (x,y) == coordinate(Latt, i), 1:size(Latt))
     (idx == nothing && forceerr) && error("Index not found!")
     return idx
